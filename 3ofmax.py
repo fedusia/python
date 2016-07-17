b72529d8bbd5854d962e7347e8fd190f49e10725
Dmitry S. Fedorov <fedorov@corp.sputnik.ru>
2016-07-17 22:42:07 +0300

    Implementation custom func max()

---
+++ 3ofmax.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+
+def inputNumber():
+    counter = 0
+    numbers = list()
+    while True:
+        num = input('Enter number:')
+        if isinstance(num, int):
+            numbers.append(num)
+            counter += 1
+        else:
+            print ('Not number: %s' % num)
+            continue
+        if counter == 3:
+            break
+    return numbers
+
+
+def max(list):
+    max = sorted(list, reverse=True)
+    return max[0]
+
+
+def main():
+    a = inputNumber()
+    b = max(a)
+    print('Max of 3 digits %s is: %i' % (a, b))
+
+if __name__ == '__main__':
+    main()
