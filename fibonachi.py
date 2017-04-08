34d89e2a8629a3c7ab1a86b4c917a709feb50323
Dmitry S. Fedorov <fedusia@yandex-team.ru>
2017-04-08 15:40:53 +0300

    fib

---
+++ fibonachi.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def inputNumber():
+    num = int(input('Enter number:'))
+    return num
+
+def fibonachi(num):
+    
+
+
+def main():
+    a = inputNumber()
+    print (a)
+
+
+if __name__ == '__main__':
+  main()
+
