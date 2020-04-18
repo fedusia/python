c6c2b3802b22a700ee024110877da19609edda73
Dmitry S. Fedorov <fedusia@yandex-team.ru>
2020-04-19 01:44:51 +0300

    https://leetcode.com/problems/valid-parentheses/

---
+++ leetcode/valid-parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        st = []
+        brakets = {
+            "(": ")",
+            "[": "]",
+            "{": "}"
+        }
+        for i in s:
+            if i in ["{", "(", "["]:
+                st.append(i)
+            elif st and i == brakets[st[-1]]:
+                st.pop()
+            else:
+                return False
+        return st == []
+
+
+if __name__ == '__main__':
+    assert Solution().isValid("([)]") is False
+    assert Solution().isValid("{[]}") is True
+    assert Solution().isValid("(]") is False
+    assert Solution().isValid("]") is False
+    assert Solution().isValid("(])") is False
